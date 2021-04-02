e4e68e6e11150e5d7739ca39a1682d1780aef667
Jason <adaglo524@gmail.com>
2021-04-02 16:50:17 +0200

    book list and extra CSS addes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React, {useContext} from 'react';
 import { BookContext } from '../contexts/BookContext';
 
 const Navbar = () => {
-  const {books} =  useContext(BookContext);
+  const { books } =  useContext(BookContext);
     return ( 
         <div className="navbar">
             <h1>Ninja Reading List</h1>
